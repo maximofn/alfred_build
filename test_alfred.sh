b3ecab24dfe14a2114c871220b0365c6eec6c8a7
maximofn <maximofn@gmail.com>
2023-02-27 17:51:27 +0100

    En el script de test de alfred en ubuntu se imprime si alfred está bien o no instaldo. En el postinst de ubuntu se comprueba si existe /usr/src/alfred y si existe se borra

--- test_alfred.sh
+++ test_alfred.sh
@@ -3,4 +3,10 @@
 # Build ubuntu 20.04 docker image
 cd alfred_build
 docker build -f docker/Dockerfile.ubuntu_test -t maximofn/ubuntu_20_04_test:0.0.1 ../
-docker run -it --rm maximofn/ubuntu_20_04_test:0.0.1 /usr/src/alfred/alfred.py
+# Check if alfred is well installed
+alfred_ubuntu=$(docker run -it --rm maximofn/ubuntu_20_04_test:0.0.1 /usr/src/alfred/alfred.py)
+if [[ $alfred_ubuntu == *"You need to set your OpenAI API key in the OPENAI_API_KEY environment variable"* ]]; then
+    echo -e "\033[0;32mAlfred is well installed in ubuntu 20.04\033[0m"     # Green
+else
+    echo -e "\033[0;31mAlfred is not well installed in ubuntu 20.04\033[0m" # Red
+fi
