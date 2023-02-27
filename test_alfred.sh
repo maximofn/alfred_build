1c21cc16b1276bf1042be54ae8a3b339b3649b51
maximofn <maximofn@gmail.com>
2023-02-27 17:03:41 +0100

    Se consigue hacer el .deb para debian

--- test_alfred.sh
+++ test_alfred.sh
@@ -3,4 +3,4 @@
 # Build ubuntu 20.04 docker image
 cd alfred_build
 docker build -f docker/Dockerfile.ubuntu_test -t maximofn/ubuntu_20_04_test:0.0.1 ../
-docker run -it --rm maximofn/ubuntu_20_04_test:0.0.1
+docker run -it --rm maximofn/ubuntu_20_04_test:0.0.1 /usr/src/alfred/alfred.py
