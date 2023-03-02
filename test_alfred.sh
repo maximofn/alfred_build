5a1be39ee5ca3f08a3e5321305fa0296b4014ba6
maximofn <maximofn@gmail.com>
2023-03-02 09:48:55 +0100

    Se arregla el test de ubuntu

--- test_alfred.sh
+++ test_alfred.sh
@@ -8,7 +8,7 @@ docker build -f docker/Dockerfile.ubuntu_test2 -t maximofn/ubuntu_20_04_test2:0.
 docker build -f docker/Dockerfile.ubuntu_test3 -t maximofn/ubuntu_20_04_test3:0.0.1 ../
 
 # Test ubuntu 20.04 test 1 docker image
-alfred_ubuntu=$(docker run -it --rm maximofn/ubuntu_20_04_test1:0.0.1 /usr/src/alfred/alfred.py)
+alfred_ubuntu=$(echo API_KEY | docker run -i --rm maximofn/ubuntu_20_04_test1:0.0.1 /usr/src/alfred/alfred.py)
 if [[ $alfred_ubuntu == *$ALFRED_MESSAGE* ]]; then
     echo -e "\033[0;32mAlfred is well installed in ubuntu 20.04 test 1\033[0m"     # Green
 else
@@ -16,16 +16,16 @@ else
 fi
 
 # Test ubuntu 20.04 test 2 docker image
-alfred_ubuntu=$(docker run -it --rm maximofn/ubuntu_20_04_test2:0.0.1 /usr/src/alfred/alfred.py)
-if [[ $alfred_ubuntu == *"You need to set your OpenAI API key in the OPENAI_API_KEY environment variable"* ]]; then
+alfred_ubuntu=$(echo API_KEY | docker run -i --rm maximofn/ubuntu_20_04_test1:0.0.1 /usr/src/alfred/alfred.py)
+if [[ $alfred_ubuntu == *$ALFRED_MESSAGE* ]]; then
     echo -e "\033[0;32mAlfred is well installed in ubuntu 20.04 test 2\033[0m"     # Green
 else
     echo -e "\033[0;31mAlfred is not well installed in ubuntu 20.04 test 2\033[0m" # Red
 fi
 
 # Test ubuntu 20.04 test 3 docker image
-alfred_ubuntu=$(docker run -it --rm maximofn/ubuntu_20_04_test3:0.0.1 /usr/src/alfred/alfred.py)
-if [[ $alfred_ubuntu == *"You need to set your OpenAI API key in the OPENAI_API_KEY environment variable"* ]]; then
+alfred_ubuntu=$(echo API_KEY | docker run -i --rm maximofn/ubuntu_20_04_test1:0.0.1 /usr/src/alfred/alfred.py)
+if [[ $alfred_ubuntu == *$ALFRED_MESSAGE* ]]; then
     echo -e "\033[0;32mAlfred is well installed in ubuntu 20.04 test 3\033[0m"     # Green
 else
     echo -e "\033[0;31mAlfred is not well installed in ubuntu 20.04 test 3\033[0m" # Red
