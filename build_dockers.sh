e82197d657394bf29300165b757fe65e71829c00
maximofn <maximofn@gmail.com>
2023-03-02 11:04:54 +0100

    Se actualizan los archivos para la versión v1.3

--- build_dockers.sh
+++ build_dockers.sh
@@ -3,4 +3,4 @@
 # Build ubuntu 20.04 docker image
 rm ubuntu20.04/alfred.deb
 docker build -f docker/Dockerfile.ubuntu_build -t maximofn/ubuntu_20_04_build:0.0.1 ../
-docker run -it --rm -v ./debian/:/mnt maximofn/ubuntu_20_04_build:0.0.1 cp /home/alfred.deb /mnt
+docker run -it --rm -v ./debian/:/mnt maximofn/ubuntu_20_04_build:0.0.1 cp /home/alfredv1_3.deb /mnt
