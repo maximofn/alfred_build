1f9686af6d70aa1146463823ecace035aee35df3
maximofn <maximofn@gmail.com>
2023-03-05 02:18:14 +0100

    He borrado las imágenes de docker de mi ordenador y al volver a generarlas se producian errores. Modifico todo lo que producia estos errores y lo subo

--- build_dockers.sh
+++ build_dockers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Build ubuntu 20.04 docker image
-rm ubuntu20.04/alfred.deb
+rm debian/alfredv1_3.deb
 docker build -f docker/Dockerfile.ubuntu_build -t maximofn/ubuntu_20_04_build:0.0.1 ../
 docker run -it --rm -v ./debian/:/mnt maximofn/ubuntu_20_04_build:0.0.1 cp /home/alfredv1_3.deb /mnt
